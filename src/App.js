e4b6e8f4b28388ca04f45f41e7333233bcc6550b
Djamalutin <fisher.gerryy@mail.ru>
2023-06-03 15:13:15 +0300

    creating list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,146 @@
-import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { MainContainer } from "./MainCointainer";
 
 function App() {
-  return <div className="App"></div>;
+  const [coins, setCoins] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  async function fetchCoins() {
+    try {
+      const { data } = await axios.get(
+        `https://api.coinstats.app/public/v1/coins?limit=10`
+      );
+      setCoins(data.coins);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
+  if (loading) {
+    return (
+      <MainContainer>
+        <h3>Loading...</h3>
+      </MainContainer>
+    );
+  }
+
+  if (error) {
+    return (
+      <MainContainer>
+        <h3>Getting data error...</h3>
+      </MainContainer>
+    );
+  }
+
+  if (!coins) {
+    return (
+      <MainContainer>
+        <h3>No data...</h3>
+      </MainContainer>
+    );
+  }
+
+  return (
+    <MainContainer>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          height: "40px",
+          border: "2px solid black",
+          borderRadius: "15px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "10%",
+            height: "100%",
+            borderRight: "1px solid black",
+          }}
+        >
+          <span>№</span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "40%",
+            height: "100%",
+            borderRight: "1px solid black",
+          }}
+        >
+          <span>Naming</span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "50%",
+            height: "100%",
+          }}
+        >
+          <span>Price</span>
+        </div>
+      </div>
+      <ul style={{ padding: 0, margin: 0, listStyleType: "none" }}>
+        {coins.map((coin) => {
+          return (
+            <li key={coin.id} style={{ display: "flex" }}>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "10%",
+                  height: "100%",
+                  borderRight: "1px solid black",
+                }}
+              >
+                <span>{coin.rank}</span>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "40%",
+                  height: "100%",
+                  borderRight: "1px solid black",
+                }}
+              >
+                <span>{coin.name}</span>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "50%",
+                  height: "100%",
+                  borderRight: "1px solid black",
+                }}
+              >
+                <span>{coin.price}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </MainContainer>
+  );
 }
 
 export default App;
---
+++ src/MainCointainer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const MainContainer = ({ children }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "550px",
+          height: "500px",
+          borderRadius: "15px",
+          padding: "20px",
+          backgroundColor: "rgb(228, 229, 151)",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
